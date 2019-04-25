7897ed90d8589820f908ca0149464cfb2985c79b
GurmailSinghSachdeva <gggg.4mail@gmail.com>
2019-04-26 00:47:04 +0530

    base module changes

--- base/src/main/java/com/dev/widgets/MyImageView.kt
+++ base/src/main/java/com/dev/widgets/MyImageView.kt
@@ -1,13 +1,18 @@
 package com.dev.widgets
 
 import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.transition.Transition
 
 open class MyImageView(context: Context, attributeSet: AttributeSet?) : AppCompatImageView(context, attributeSet) {
 
-    fun setImageUri(url: String) {
-        Glide.with(this).load(url).into(this)
+    fun setImageUri(url: String, placeholderImage: ColorDrawable) {
+        Glide.with(this).load(url).placeholder(placeholderImage).error(placeholderImage)
+            .transition(DrawableTransitionOptions.withCrossFade(200))
+            .into(this)
     }
 }
--- base/src/main/java/com/dev/widgets/MyRecyclerView.kt
+++ base/src/main/java/com/dev/widgets/MyRecyclerView.kt
@@ -2,6 +2,7 @@ package com.dev.widgets
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.recyclerview.widget.GridLayoutManager
 import com.airbnb.epoxy.EpoxyRecyclerView
 
-class MyRecyclerView(context: Context, attributeSet: AttributeSet): EpoxyRecyclerView(context, attributeSet)
+class MyRecyclerView(context: Context, attributeSet: AttributeSet) : EpoxyRecyclerView(context, attributeSet)
